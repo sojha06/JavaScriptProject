e014d511bb4eb7c125d6f54cc3d39b84939e3401
Shivam Ojha <sojha06@gmail.com>
2025-06-19 20:46:45 +0530

    Added Functions

--- Controller.js
+++ Controller.js
@@ -1,5 +1,24 @@
-function greet(name){
+function greetHello(name){
     return "Hello " + name + "!";
 }
+console.log(greetHello("Shivam"));
 
-console.log(greet("Shivam"));
+const greetHi = function(name){
+    console.log('Hi ' + name + '!');
+};
+greetHi('Megha');
+
+const greetHiWithArrow = (name) => {
+    console.log('Hi ' + name + ' With Arrow');
+};
+greetHiWithArrow('Megha');
+
+const obj = {
+    name : "Megha",
+    regular : function(){
+        console.log("regular Hi " + this.name);
+    },
+    withArrow : () => {
+        console.log("regular Hi " + this.name);
+    }
+};
